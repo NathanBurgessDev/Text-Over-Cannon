cc672039d901c8ae3d04cd7e4ebefe5d19634130
Joe Heyes <josephheyes@gmail.com>
2024-03-02 19:39:43 +0000

    motor on pi test 4

--- gates/src/main.py
+++ gates/src/main.py
@@ -2,7 +2,9 @@ from gpiozero import Motor
 
 def main():
     motor = Motor(17, 22)
-    motor.stop()
+    while True:
+        motor.forward()
+        print("forward :)")
 
 if __name__ == "__main__":
     main()
